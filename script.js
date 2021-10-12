117b386bf6306175d28ae83902af93dfdb5ec8fd
Y00NMIN <89017779+Y00NMIN@users.noreply.github.com>
2021-10-12 13:23:27 +0000

    none JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const text_button = document.querySelector('main .main_title .text_button');
+const main_title_word = document.querySelector('main .main_title .main_title_word');
+
+text_button.addEventListener('click', () => {
+  text_button.classList.toggle('clicked');
+  main_title_word.classList.toggle('clamp');
+});
