c099cb211059ea1baf9c837c29983464a1522572
Y00NMIN <89017779+Y00NMIN@users.noreply.github.com>
2021-10-23 11:44:59 +0000

    addeventlistner에 함수값이 배정이안된다.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const text_button = document.querySelector('main .main_title .text_button');
 const main_title_word = document.querySelector('main .main_title .main_title_word');
 
-text_button.addEventListener('click', () => {
+text_button.addEventListener('click', toggle); 
+function toggle() {
   text_button.classList.toggle('clicked');
   main_title_word.classList.toggle('clamp');
-});
+};
