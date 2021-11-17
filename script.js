cc732200f5cc359b03dc214e799b089f5aab3181
Sungmin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-11-17 14:31:17 +0000

    toggle events 수정

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const text_button = document.querySelector('main .main_title .text_button');
-const main_title_word = document.querySelector('main .main_title .main_title_word');
 
 window.onload = function(){
-  const text_button = document.querySelector('main .main_title .text_button');
-  const main_title_word = document.querySelector('main .main_title .main_title_word');
+  const text_button = document.querySelector('.main .main_title .text_button');
+  const main_title_word = document.querySelector('.main .main_title .main_title_word');
 
   text_button.addEventListener('click', ()=>{
     text_button.classList.toggle('clicked');
